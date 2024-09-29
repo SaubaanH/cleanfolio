e813830e89f087d992d993ef9eec63dbba7924f5
Saubaan Hasan <106204098+SaubaanH@users.noreply.github.com>
2024-09-29 18:51:02 -0400

    Updated project with custom domain

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://saubaan.me',
+  title: 'Portfolio',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Saubaan',
+  role: 'Software Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'I’m a Computer Engineering student at the University of Waterloo with a passion for technology and coding since I was 9. I love building efficient, user-focused applications and working with both software and hardware to solve complex problems and create impactful digital solutions.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/saubaan/',
+    github: 'https://github.com/SaubaanH',
   },
 }
 
@@ -21,23 +21,23 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Cluo',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'A Remote Desktop Protocol (RDP) application for iOS, macOS, and Android, providing secure and seamless remote access across platforms with dynamic server management.',
     stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    sourceCode: 'https://github.com/FreeRDP/FreeRDP',
+    livePreview: 'https://www.mazzzing.com/cluoapp',
   },
   {
-    name: 'Project 2',
+    name: 'HomieHub',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'A matchmaking video chat application that pairs users based on personality traits, offering real-time video chats with icebreaker prompts, and won first overall at HackWithACrew for its innovation.',
     stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    sourceCode: 'https://github.com/SaubaanH/Homie-Hub',
+    livePreview: 'https://devpost.com/software/homie-hub',
   },
   {
-    name: 'Project 3',
+    name: 'Some AI Project',
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
     stack: ['SASS', 'TypeScript', 'React'],
@@ -65,7 +65,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'saubaanhasan@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
